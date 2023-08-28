979f94601adb263d243634da248f8fcbad05f41d
Bogdan <butakovbi@gmail.com>
2023-08-28 13:59:15 +0300

    JWT

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timedelta
 from fastapi import FastAPI, Cookie, Response, HTTPException, Request, Depends, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import HTTPBasic, OAuth2PasswordBearer
 import re
+import jwt
 from data import *
 from models import Feedback
 
@@ -9,6 +11,10 @@ app = FastAPI(
 )
 
 security = HTTPBasic()
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
+SECRET_KEY = "abc123xyz456"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 # @app.post("/create_user")
 # async def create_user(user: UserCreate):
@@ -42,15 +48,15 @@ async def search_product(keyword: str, category: str = None, limit: int = 10):
     return ans[:limit]
 
 
-@app.post('/login')
-async def login(user: User, response: Response):
-    for person in users:
-        if person.name == user.username and person.password == user.password:
-            session_token = "abc123xyz456"
-            sessions[session_token] = user
-            response.set_cookie(key="session_token", value=session_token, httponly=True)
-            return {"message": "Set cookie"}
-    return {"message": "Invalid username or password"}
+# @app.post('/login')
+# async def login(user: User, response: Response):
+#     for person in users:
+#         if person.name == user.username and person.password == user.password:
+#             session_token = "abc123xyz456"
+#             sessions[session_token] = user
+#             response.set_cookie(key="session_token", value=session_token, httponly=True)
+#             return {"message": "Set cookie"}
+#     return {"message": "Invalid username or password"}
 
 
 @app.get("/user")
@@ -85,14 +91,40 @@ def get_user_from_db(username: str):
     return None
 
 
-def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
-    user = get_user_from_db(credentials.username)
-    if user is None or user.username != credentials.username or user.password != credentials.password:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+def authenticate_user(username, password):
+    user = get_user_from_db(username)
+    if user is None or user.password != password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid credentials",
+                            headers={"WWW-Authenticate": "Bearer"})
     return user
 
 
-@app.get("/login")
-async def protected_login(user: User = Depends(authenticate_user)):
-    return {"message": "You got my secret, welcome",
-            "user_info": user}
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
+    return encoded_jwt
+
+
+@app.post("/login")
+async def login(user: User):
+    authenticate_user(user.username, user.password)
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.post("/protected_resource")
+async def protected_resource(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm="HS256")
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired", headers={"WWW-Authenticate": "Bearer"})
+    except jwt.DecodeError:
+        raise HTTPException(status_code=401, detail="Invalid token", headers={"WWW-Authenticate": "Bearer"})
+
+    return {"message": "Access granted to protected resource"}
